a17d4798678460ca5caf9f0b9fe7a557258eddf3
Andry-code-web <73274663@certus.edu.pe>
2025-09-05 17:56:51 -0500

    Add async delete review functionality and improve imports

--- lab1/reviews/views.py
+++ lab1/reviews/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import redirect, render
 from .models import Review
 from .forms import ReviewForm
 
+from asgiref.sync import sync_to_async
+
 # Create your views here.
 
 def index(request):
@@ -22,3 +24,7 @@ def add_review(request):
     else:
         form = ReviewForm()
     return render(request, 'add_review.html', {'form': form})
+
+async def delete_review(request, review_id):
+    await sync_to_async(Review.objects.filter(id=review_id).delete)()
+    return redirect('list_reviews')
